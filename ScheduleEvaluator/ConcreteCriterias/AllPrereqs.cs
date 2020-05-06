0500e4c4f26a6995493d9865ae94550c47c60fff
nguyendb <nguyendb@uw.edu>
2020-05-05 19:37:35 -0700

    Implemented AllPrereqs Criterion

--- ScheduleEvaluator/ConcreteCriterias/AllPrereqs.cs
+++ ScheduleEvaluator/ConcreteCriterias/AllPrereqs.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ScheduleEvaluator.ConcreteCriterias
 {
@@ -8,12 +9,56 @@ namespace ScheduleEvaluator.ConcreteCriterias
 
     public class AllPrereqs : Criteria
     {
+        private Evaluator e;
+
         public AllPrereqs(double weight) : base(weight) {
+            e = new Evaluator();
         }
 
         public override double getResult(ScheduleModel s)
         {
-            throw new NotImplementedException();
+            HashSet<string> completedCourses = new HashSet<string>();
+
+            // Sort quarters from earliest to latest
+            List<Quarter> quarters = s.Quarters;
+            quarters.Sort();
+
+            int invalidCourses = 0;
+
+            foreach (Quarter q in quarters)
+            {
+                // Iterate over courses twice to concurrent classes from being
+                // seen as completed prerequisites
+                foreach (Course c in q.Courses)
+                {
+                    // First check if prereqs are met
+                    if (!verifyPrereqs(c.Id, completedCourses)) invalidCourses++;
+                }
+
+                // Then add to completed courses
+                foreach (Course c in q.Courses) completedCourses.Add(c.Id);
+            }
+
+            return (invalidCourses > 0 ? 0 : 1) * weight;
+        }
+
+        // Verifies completion of a course's prereqs
+        private bool verifyPrereqs(string courseId, HashSet<string> complete)
+        {
+            List<CourseNode> prereqs = null;
+            Task.Run(async() =>
+            {
+                prereqs = await e.getCourseNetwork(courseId);
+            }).GetAwaiter().GetResult();
+
+            if (prereqs == null) throw new Exception("Could not get CourseNetwork");
+
+            // Verify that each course's prereqs have been completed
+            foreach (CourseNode cn in prereqs)
+            {
+                if (!complete.Contains(cn.courseID)) return false;
+            }
+            return true;
         }
     }
 }
