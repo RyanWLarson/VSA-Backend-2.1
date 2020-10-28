49020c7fc7bec19a4bbe5ac842e49f9d5d683b74
jadonc <46636295+jadonc@users.noreply.github.com>
2020-10-27 23:37:33 -0700

    Update DBConnection.cs documentation...
    Also added a readability suggestion so other developers can understand immediately.

--- ScheduleEvaluator/DBConnection.cs
+++ ScheduleEvaluator/DBConnection.cs
@@ -15,22 +15,48 @@ namespace ScheduleEvaluator
         // 
         //------------------------------------------------------------------------------
 
+        // a C# 'DataTable' represents "one table of in-memory data" (see .NET docs)
+        // 'ExecuteToDT' essentially means:
+        //      - execute SQL command
+        //      - store execution result in a DataTable object
+        //      - return the now-populated DataTable object
         public DataTable ExecuteToDT(string query)
         {
             OpenSQLConnection();
+            
+            // construct an SQL command object
             SqlCommand cmd = new SqlCommand(query, myConnection);
+            
+            // empty DataTable; will hold the results returned by the command
             DataTable dt = new DataTable();
+            
+            // [COMMENT Oct. 27, 2020]:
+            // this nested 'using' block should be rewritten just a tad bit, because
+            // the variable <con> is not used at all, and command creation is redundant.
+            // making use of the vars created with 'using' will eliminate
+            // redundancy and improve operational efficiency and memory usage. thanks!
             using (var con = myConnection)
             {
                 using (var command = new SqlCommand(query))
                 {
                     myConnection.Open();
+                    
+                    // a C# 'SqlDataReader' (see .NET docs):
+                    // "Provides a way of reading a forward-only'
+                    // 'stream of rows from a SQL Server database."
+                    //
+                    // 'ExecuteReader()' (see .NET docs):
+                    // sends CommandText to the Connection, builds a SqlDataReader,
+                    // and returns this reader WITH execution results/values/data.
                     using (SqlDataReader dr = cmd.ExecuteReader())
                     {
+                        // populate the temporary DataTable with the SqlDataReader's results
                         dt.Load(dr);
                     }
                 }
             }
+            
+            // cleanup and return the newly populated DataTable
             myConnection.Close();
             return dt;
         }
